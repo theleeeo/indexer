c8328e666ba5c0dddee6487e5d7a9fb1ed0d0c59
Leonard Forsberg <leonard.e.forsberg@gmail.com>
2026-01-25 19:31:54 +0100

    reorder and rename job result log

--- jobqueue/finish.go
+++ jobqueue/finish.go
@@ -11,7 +11,7 @@ import (
 )
 
 func (w *Worker) finish(ctx context.Context, group string, job Job, runErr error) error {
-	logger := slog.With("group", group, "job_id", job.ID, "job_type", job.Type)
+	logger := slog.With("group", group, "type", job.Type, "id", job.ID)
 
 	if runErr != nil {
 		logger.Error("job failed", "error", runErr)
